c5b1039628c47e7e132c9eddb8fd6db486d013fa
kamask <s@sha88.ru>
2020-01-10 00:43:55 +0300

    Added get tasks, get tasks info, send telegram bot partial task info

--- KskTeleBot.js
+++ KskTeleBot.js
@@ -1,3 +1,4 @@
+const {startPass} = require('./data')
 const Telegraf = require('telegraf')
 const Markup = require('telegraf/markup')
 
@@ -7,13 +8,29 @@ module.exports = class self{
 
     constructor(token) {
         this.#bot = new Telegraf(token)
+        let delPassReq
         this.#bot.start(ctx => {
-            this.#chatId = ctx.update.message.chat.id
+            delPassReq = ctx.message.message_id
+            this.#bot.telegram.sendMessage(ctx.message.chat.id,'Назови своё любимое число)').then(ctx => {
+                this.#bot.telegram.deleteMessage(ctx.chat.id, delPassReq)
+                delPassReq = ctx.message_id
+            })
+        })
+        this.#bot.hears(startPass, ctx => {
+            this.#chatId = ctx.message.chat.id
+            this.send('Ща всё будет)\nУдачи!')
+            this.delete(delPassReq)
+            this.delete(ctx.message.message_id)
         })
         this.#bot.launch()
     }
 
-    send(t){
-        this.#bot.telegram.sendMessage(this.#chatId, t)
+    send(t, o = undefined){
+        if(!this.#chatId) return false
+        return this.#bot.telegram.sendMessage(this.#chatId, t, o)
+    }
+
+    delete(msg){
+        this.#bot.telegram.deleteMessage(this.#chatId, msg)
     }
 }
--- KskYoudoTask.js
+++ KskYoudoTask.js
@@ -2,28 +2,62 @@ function log(...p){console.log(...p)}
 const {youdo} = require('./config')
 const KskWD = require('./KskWebDriver')
 
-
-module.exports = class self{
+class self{
     static #wd = null
+    static #process = false
 
     id
+    info
 
     static async init(){
+        self.#process = true
         self.#wd = await KskWD.build('chrome')
-        await self.#wd.youdoAuth(youdo, 'Get Task')
-        return self.#wd
+        await self.#wd.youdoAuth(youdo, 'Get Task Info')
+        self.#process = false
+        return self
     }
 
     static async build(id){
-        if(!self.#wd) await self.init()
-        return new self(id)
+        log('\n---\nNew task - ' + id)
+        if(!self.#wd && !self.#process) await self.init()
+        if(self.#process){
+            await new Promise(resolve => {
+                setInterval(()=>{
+                    if(!self.#process){
+                        self.#process = true
+                        resolve()
+                    }
+                },100)
+            })
+        }
+        const d = self.#wd
+        log('Task info getting - ' + id + ' ...')
+        await d.open('https://youdo.com/t' + id)
+        const info = {
+            title: await (await d.cssLocated('.b-task-block__header__title')).getText(),
+            text: await (await d.cssLocated('.b-task-block__description')).getText(),
+            address: await (await d.cssLocated('.b-task-block__address > .b-task-block__info')).getText(),
+            date: await (await d.cssLocated('.b-task-block__date__wrap')).getText(),
+            price: await (await d.cssLocated('.b-task-block__budget > .b-task-block__info')).getText(),
+            priceMethod: await (await d.cssLocated('.b-task-block__payment > .b-task-block__info')).getText(),
+            place: await (await d.cssLocated('.b-task-block__location > .b-task-block__info')).getText(),
+            name: await (await d.cssLocated('.b-task-block__userinfo__name')).getText(),
+            authorId: (await (await d.cssLocated('.b-task-block__userinfo__name')).getAttribute('href')).substr(2)
+        }
+        log('Task info getting done! - ' + id + '\n---')
+        return new self(id, info)
     }
 
-    constructor(id){
+    constructor(id, info){
         this.id = id
+        this.info = info
     }
 
     sendToTelegramBot(t){
-        t.send(this.id)
+        t.send(this.info.title)
     }
 }
+
+self.init()
+
+module.exports = self
