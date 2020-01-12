b49b59cc650f92783268797a3c9ca7c2fcb69fb7
kamask <s@sha88.ru>
2020-01-12 07:51:21 +0300

    1 день полёт нормальный, но добавил предварительную отправку сообщения в телеграм, перед парсингом основной инфы задания

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 global.storage = new Map()
 const KskWD = require('./KskWebDriver')
 const Task = require('./KskYoudoTask')
+const tBot = require('./KskTeleBot')
 
 KskWD.build('chrome').then(async wd => {
 
@@ -22,17 +23,21 @@ KskWD.build('chrome').then(async wd => {
 
             let ltLink = await wd.cssLocated('li.listItem___a431d:nth-child(2) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)')
             let ltId = await ltLink.getAttribute('data-id')
+            let ltText = await ltLink.getText()
 
             if(!currentLtId){
                 currentLtId = ltId
+                tBot.send('<a href="https://youdo.com/t'+ltId+'">'+ltText+'</a>', { disable_web_page_preview: true, parse_mode: 'HTML'})
                 ;(await Task.build(ltId)).sendToTelegramBot()
             }
 
             let i = 3, newTasksId = []
             while(currentLtId < ltId){
+                tBot.send('<a href="https://youdo.com/t'+ltId+'">'+ltText+'</a>', { disable_web_page_preview: true, parse_mode: 'HTML'})
                 newTasksId.push(ltId)
                 ltLink = await wd.cssLocated('li.listItem___a431d:nth-child(' + i++ + ') > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)')
                 ltId = await ltLink.getAttribute('data-id')
+                ltText = await ltLink.getText()
             }
 
             if(newTasksId.length > 0){
--- textNewTask.js
+++ textNewTask.js
@@ -8,8 +8,7 @@ module.exports = function ({id, title, text, date, address, price, priceMethod, 
 ${text}
 
 -------------------
-${date[0]}: ${date[1]}
-${date[2] || ''}: ${date[3] || ''}
+${date[0]+': '+date[1]+(date[3] ? ('\n'+date[2]+': '+date[3]) : '')}
 -------------------
 
 <a href="https://yandex.ru/maps/?text=${address}">${address}</a>
