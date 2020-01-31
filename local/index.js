7df0b61e752a68be6da98d0d3bba48748fadd417
KamaSK <s@sha88.ru>
2020-02-01 01:01:01 +0300

    Refactor to partial procedure style and added restarted main-loop for pass error Optimized get tasks with execute script in devtools

--- local/index.js
+++ local/index.js
@@ -22,104 +22,130 @@ let wdGT, wdTA
 
     /*--------------------- INIT and AUTH   --------------------------------------------*/
 
-    wdGT = await new WD().building('chrome') // GT - Get Task
-    //wdGT.d.manage().window().minimize()
-    wdGT.d.get('https://youdo.com')
+    async function build() {
+        wdGT = await new WD().building('chrome') // GT - Get Task
+        //wdGT.d.manage().window().minimize()
+        wdGT.d.get('https://youdo.com')
 
-    wdTA = await new WD().building('chrome') // TA - Task Answer
-    //wdTA.d.manage().window().minimize()
-    wdTA.d.get('https://youdo.com')
+        wdTA = await new WD().building('chrome') // TA - Task Answer
+        //wdTA.d.manage().window().minimize()
+        wdTA.d.get('https://youdo.com')
+    }
+
+    await build()
 
     let auth, authCount = 0, authTrying = 0
 
-    do{
-        log('Auth trying - ' + (++authTrying))
-        await wdGT.d.get('https://youdo.com')
-        await wdGT.d.executeScript(async () => {
-            return await fetch('/api/users/signin/', {
-                headers: {
-                    "accept": "application/json",
-                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-                },
-                method: 'POST',
-                body: 'login='+arguments[0]+'&password='+arguments[1]
-            })
-        }, process.env.YOUDO_LOGIN, process.env.YOUDO_PASS)
-        await wdGT.reload(3000)
-        try{
-            auth = await wdGT.d.executeScript(()=>window.YouDo.GuardCheck.isAuth)
-        }catch(e){
-            continue
-        }
-    }while(!auth)
+    async function authorizing() {
+        do{
+            log('Auth trying - ' + (++authTrying))
+            await wdGT.d.get('https://youdo.com')
+            await wdGT.d.executeScript(async () => {
+                return await fetch('/api/users/signin/', {
+                    headers: {
+                        "accept": "application/json",
+                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+                    },
+                    method: 'POST',
+                    body: 'login='+arguments[0]+'&password='+arguments[1]
+                })
+            }, process.env.YOUDO_LOGIN, process.env.YOUDO_PASS)
+            await wdGT.reload(3000)
+            try{
+                auth = await wdGT.d.executeScript(()=>window.YouDo.GuardCheck.isAuth)
+            }catch(e){
+                continue
+            }
+        }while(!auth)
 
-    log('WebDriver: Task Watcher - auth!')
-    authCount++
-
-    const cookies = await wdGT.d.manage().getCookies()
-    for(let i of cookies) await wdTA.d.manage().addCookie(i)
-
-    await wdTA.reload(500)
-
-    if(await wdTA.d.executeScript(()=>window.YouDo.GuardCheck.isAuth)){
-        log('WebDriver: Task Answer - auth!')
+        log('WebDriver: Task Watcher - auth!')
         authCount++
+
+        const cookies = await wdGT.d.manage().getCookies()
+        for(let i of cookies) await wdTA.d.manage().addCookie(i)
+
+        await wdTA.reload(500)
+
+        if(await wdTA.d.executeScript(()=>window.YouDo.GuardCheck.isAuth)){
+            log('WebDriver: Task Answer - auth!')
+            authCount++
+        }
     }
+
+    await authorizing()
 
 
     /*--------------------- Get Tasks and send to server   --------------------------------------------*/
 
 
-    while(authCount === 2){
-        try{
+    ;(async function mainloop(){
+        while(authCount === 2){
+            try{
 
-            await wdGT.d.get('https://youdo.com/api/tasks/tasks/?q=&list=all&status=opened&lat=55.753215&lng=37.622504&radius=30&page=1&noOffers=false&onlySbr=false&onlyB2B=false&priceMin=&sortType=1&onlyVirtual=false&sub=30&sub=34&sub=32&sub=110&sub=84&sub=109&sub=90')
-            const tasks = JSON.parse(await (await wdGT.d.findElement({css: 'pre'})).getText()).ResultObject.Items
+                await wdGT.d.get('https://youdo.com/api/tasks/tasks/?q=&list=all&status=opened&lat=55.753215&lng=37.622504&radius=30&page=1&noOffers=false&onlySbr=false&onlyB2B=false&priceMin=&sortType=1&onlyVirtual=false&sub=30&sub=34&sub=32&sub=110&sub=84&sub=109&sub=90')
+                const tasks = await wdGT.d.executeScript(()=>{
+                    return JSON.parse(document.querySelector('pre').innerText).ResultObject.Items
+                })
 
-            let i = 0
-            while(currentLtId < tasks[i].Id){
-                if(currentLtId === 0) currentLtId = tasks[i].Id
-                log('\nNew task - ' + tasks[i].Id + ' | ' + new Date().toLocaleString('ru-RU'))
+                let i = 0
+                while(currentLtId < tasks[i].Id){
+                    if(currentLtId === 0) currentLtId = tasks[i].Id
+                    log('\nNew task - ' + tasks[i].Id + ' | ' + new Date().toLocaleString('ru-RU'))
                     const task = {
-                    id: tasks[i].Id,
-                    title: tasks[i].Name,
-                    price: tasks[i].PriceAmount,
-                    budget: tasks[i].BudgetDescription,
-                    secure: tasks[i].IsSbr,
-                    address: tasks[i].Address,
-                    userName: tasks[i].CreatorInfo.UserName,
-                    userAvatar: tasks[i].CreatorInfo.Avatar.slice(0,-6)+'H180W180',
-                    userId: tasks[i].CreatorInfo.Id,
-                    userRate: [tasks[i].CreatorInfo.Rating.PositiveReviews, tasks[i].CreatorInfo.Rating.NegativeReviews],
-                    photo: []
-                }
-                await wdGT.d.get('https://youdo.com/api/tasks/taskmodel/?taskId='+task.id)
-                let taskFull = JSON.parse(await (await wdGT.d.findElement({css: 'pre'})).getText()).ResultObject
-                task.place = taskFull.AttributesValues.Location.Concatenated
-                taskFull = taskFull.TaskData
-                task.text = taskFull.Description
-                task.date = {created: taskFull.Dates.CreationDate}
-                task.date.begin = taskFull.Dates.NeedBeginDate
-                task.date.end = taskFull.Dates.NeedEndDate
-                if(taskFull.Media.length > 0){
-                    taskFull.Media.forEach(j=>{
-                        task.photo.push('https://content3.youdo.com/get.content?ContentId='+j.Id+'&size=Max1600')
+                        id: tasks[i].Id,
+                        title: tasks[i].Name,
+                        price: tasks[i].PriceAmount,
+                        budget: tasks[i].BudgetDescription,
+                        secure: tasks[i].IsSbr,
+                        address: tasks[i].Address,
+                        userName: tasks[i].CreatorInfo.UserName,
+                        userAvatar: tasks[i].CreatorInfo.Avatar.slice(0,-6)+'H180W180',
+                        userId: tasks[i].CreatorInfo.Id,
+                        userRate: [tasks[i].CreatorInfo.Rating.PositiveReviews, tasks[i].CreatorInfo.Rating.NegativeReviews],
+                        photo: []
+                    }
+                    await wdGT.d.get('https://youdo.com/api/tasks/taskmodel/?taskId='+task.id)
+
+                    let taskFull = await wdGT.d.executeScript(()=>{
+                        return JSON.parse(document.querySelector('pre').innerText).ResultObject
                     })
+
+                    task.place = taskFull.AttributesValues.Location.Concatenated
+                    taskFull = taskFull.TaskData
+                    task.text = taskFull.Description
+                    task.date = {created: taskFull.Dates.CreationDate}
+                    task.date.begin = taskFull.Dates.NeedBeginDate
+                    task.date.end = taskFull.Dates.NeedEndDate
+                    if(taskFull.Media.length > 0){
+                        taskFull.Media.forEach(j=>{
+                            task.photo.push('https://content3.youdo.com/get.content?ContentId='+j.Id+'&size=Max1600')
+                        })
+                    }
+                    socket.emit('kskNewTask', task)
+                    i++
                 }
-                socket.emit('kskNewTask', task)
-                i++
+
+                currentLtId = tasks[0].Id
+                await wdGT.d.sleep(1000)
+
+            }catch(e){
+                console.error(e)
+                break
             }
-
-            currentLtId = tasks[0].Id
-            await wdGT.reload(1000)
-
-        }catch(e){
-            console.error(e)
-            continue
         }
-    }
+
+        auth = false
+        authCount = 0
+        authTrying = 0
+
+        await build()
+        await authorizing()
+        await mainloop()
+    })()
 
 })()
+
+
 
 socket.on('kskAnswer', async data => {
     let done = false
